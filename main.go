df23fcad577f15bcc83ecae803e29330489c2b80
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-03-08 09:12:57 +0100

    Draw only when mouse is pressed

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	_ "fmt"
 
-	"github.com/veandco/go-sdl2/gfx"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -21,7 +21,6 @@ func main() {
 	}
 	defer renderer.Destroy()
 
-	var winWidth, winHeight int32 = 800, 600
 	renderer.SetDrawColor(0, 255, 0, 255)
 	renderer.FillRect(&sdl.Rect{
 		X: 10,
@@ -32,33 +31,30 @@ func main() {
 
 	points := []sdl.Point{}
 	running := true
+	pressed := false
 	for running {
 		renderer.SetDrawColor(0, 0, 0, 255)
 		renderer.Clear()
+
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
 				running = false
 			case *sdl.MouseMotionEvent:
 				// fmt.Println("Mouse", t.Which, "moved by", t.XRel, t.YRel, "at", t.X, t.Y)
-				points = append(points, sdl.Point{X: t.X, Y: t.Y})
-				// case *sdl.MouseButtonEvent:
-				// 	if t.State == sdl.PRESSED {
-				// 		fmt.Println("Mouse", t.Which, "button", t.Button, "pressed at", t.X, t.Y)
-				// 	} else {
-				// 		fmt.Println("Mouse", t.Which, "button", t.Button, "released at", t.X, t.Y)
-				// 	}
-				// }
+				if pressed {
+					points = append(points, sdl.Point{X: t.X, Y: t.Y})
+				}
+			case *sdl.MouseButtonEvent:
+				if t.State == sdl.PRESSED {
+					pressed = true
+					fmt.Println("Mouse", t.Which, "button", t.Button, "pressed at", t.X, t.Y)
+				} else {
+					pressed = false
+					fmt.Println("Mouse", t.Which, "button", t.Button, "released at", t.X, t.Y)
+				}
 			}
 		}
-		var vx, vy = make([]int16, 3), make([]int16, 3)
-		vx[0] = int16(winWidth / 3)
-		vy[0] = int16(winHeight / 3)
-		vx[1] = int16(winWidth * 2 / 3)
-		vy[1] = int16(winHeight / 3)
-		vx[2] = int16(winWidth / 2)
-		vy[2] = int16(winHeight * 2 / 3)
-		gfx.FilledPolygonColor(renderer, vx, vy, sdl.Color{R: 255, G: 0, B: 0, A: 255})
 		renderer.SetDrawColor(255, 0, 0, 255)
 		for i := 0; i < len(points)-1; i++ {
 			renderer.DrawLine(points[i].X, points[i].Y, points[i+1].X, points[i+1].Y)
