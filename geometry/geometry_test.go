05f1bedc1ffc5960b059dce153668b364850d9c7
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-04-11 19:25:51 +0200

    Interesection between segments work when segments are parallel

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -56,9 +56,27 @@ func TestIntersection(t *testing.T) {
 
 }
 
-func TestIntersectionParallel(t *testing.T) {
+func TestIntersectionHorizontalParallel(t *testing.T) {
 	segment1 := Segment{Point{0, 0}, Point{10, 0}}
 	segment2 := Segment{Point{0, 0}, Point{20, 0}}
+	_, err := Intersection(segment1, segment2)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestIntersectionVerticalParallel(t *testing.T) {
+	segment1 := Segment{Point{0, 0}, Point{0, 10}}
+	segment2 := Segment{Point{0, 0}, Point{0, 20}}
+	_, err := Intersection(segment1, segment2)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestIntersectionParallel(t *testing.T) {
+	segment1 := Segment{Point{0, 0}, Point{5, 5}}
+	segment2 := Segment{Point{2, 2}, Point{20, 20}}
 	_, err := Intersection(segment1, segment2)
 	if err != nil {
 		t.Error(err)
@@ -70,6 +88,28 @@ func TestIntersectionParallelNotIntersect(t *testing.T) {
 	segment2 := Segment{Point{4, 0}, Point{20, 0}}
 	_, err := Intersection(segment1, segment2)
 	if err == nil {
+		t.Fatal()
+	}
+}
+
+func TestIntersectionParallelNotIntersect2(t *testing.T) {
+	segment2 := Segment{Point{0, 0}, Point{3, 0}}
+	segment1 := Segment{Point{4, 0}, Point{20, 0}}
+	_, err := Intersection(segment1, segment2)
+	if err == nil {
+		t.Fatal()
+	}
+}
+
+func TestIntersection2(t *testing.T) {
+	segment1 := Segment{Point{2, 0}, Point{1, 0}}
+	segment2 := Segment{Point{1, 0}, Point{0, 0}}
+	inter, err := Intersection(segment1, segment2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	actual := Point{1, 0}
+	if inter != actual {
 		t.Fatal()
 	}
 }
